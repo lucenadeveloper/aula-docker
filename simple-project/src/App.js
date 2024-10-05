ca96048a91a652395794c9117e6d79a9bfd09eb9
Lucena <qralucena@gmail.com>
2024-10-05 16:49:49 -0300

    [FEATURE] - add simple-project example

---
+++ simple-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://logos-world.net/wp-content/uploads/2021/02/Docker-Logo.png" className="App-logo" alt="logo" />
+        <p>
+          Minha imagem docker funciona!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
